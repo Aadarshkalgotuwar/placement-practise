1ef95974d935538a3f780a62ef7096ce96dce719
Aadarsh kalgotuwar <90601240+Aadarshkalgotuwar@users.noreply.github.com>
2023-01-27 19:04:09 +0530

    Add files via upload

---
+++ halfpy.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class halfpy{
+    public static void main(String args[]){
+        int n = 8;
+        for(int i =1 ;i <=n ;i++){
+            for(int j=1; j<=i;j++){
+                System.out.print(i);
+            }
+           System.out.println();
+           
+            }
+           }
+        }
+    
