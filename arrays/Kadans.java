681fadcd253e464997ec0b0e65ad2b08863df622
Aadarsh kalgotuwar <90601240+Aadarshkalgotuwar@users.noreply.github.com>
2023-04-12 15:06:40 +0530

    Add files via upload

---
+++ arrays/Kadans.java
@@ -0,0 +1,23 @@
+public class Kadans {
+    public static void kadan(int numbers[]) {
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+            cs = cs + numbers[i];
+            if (cs < 0) {
+                cs = 0;
+            }
+            ms = Math.max(cs, ms);
+        }
+        System.out.println("MaxSubArray Sum is " + ms);
+
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = { -2, -3, 4, -1, -2, 4, -1, -2, 1, 5, -3 };
+        kadan(numbers);
+
+    }
+
+}
