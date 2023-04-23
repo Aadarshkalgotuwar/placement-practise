1dad8023eb2cd5f184f5f07c0535aa24ce330184
Aadarsh kalgotuwar <90601240+Aadarshkalgotuwar@users.noreply.github.com>
2023-04-23 13:00:24 +0530

    Add files via upload

---
+++ LINKED LIST/AddMiddle.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class AddMiddle {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void add(int idx, int data) {
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+        // i=idx-1; temp.next;
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    public void print() {
+
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addLast(7);
+        ll.addLast(9);
+        ll.print();
+        ll.add(2, 4);
+        ll.print();
+    }
+}
